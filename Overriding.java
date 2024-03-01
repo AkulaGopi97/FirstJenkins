6bdc77a231cb034148589eb4e5d6ea1f112f90b9
AkulaGopi97 <akulagopi1010@gmail.com>
2024-03-02 05:17:19 +0530

    third commit

---
+++ Overriding.java
@@ -0,0 +1,20 @@
+class Vehicle{  
+  
+  void run()
+   {
+     System.out.println("Vehicle is running");
+    }  
+  }
+
+  
+class Overriding extends Vehicle{  
+ void run() 
+{
+   System.out.println("Bike is running safely");
+}  
+  
+  public static void main(String args[]){  
+  Overriding  obj = new Overriding();//creating object  
+  obj.run();//calling method  
+  }  
+}  
